5a9a735a303ea384679e03a45caa71ffc480a289
nattikan <aoaeenattikan@gmail.com>
2016-07-13 16:06:35 +0700

    show Logo

--- app/src/main/java/com/example/asus/rusrun/MainActivity.java
+++ app/src/main/java/com/example/asus/rusrun/MainActivity.java
@@ -4,14 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private ImageView imageView;
+    private static final String ulrLogo = "http://swiftcodingthai.com/rus/image/logo_rus.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    } // Main Method
+
+        //Bind widget
+        userEditText        = (EditText) findViewById(R.id.editText4);
+        passwordEditText    = (EditText) findViewById(R.id.editText5);
+        imageView           = (ImageView) findViewById(R.id.imageView6);
+
+        //Load Imae from Server
+        Picasso.with(this).load(ulrLogo).into(imageView);
+    }   // Main Method
 
     public void clikeSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this,SignUpActivity.class));
