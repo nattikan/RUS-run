1cb8c343978568533b0a14c2855b02a83882b4d2
nattikan <aoaeenattikan@gmail.com>
2016-07-13 16:29:58 +0700

    Finish july 13

--- app/src/main/java/com/example/asus/rusrun/MainActivity.java
+++ app/src/main/java/com/example/asus/rusrun/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText userEditText, passwordEditText;
     private ImageView imageView;
     private static final String ulrLogo = "http://swiftcodingthai.com/rus/image/logo_rus.png";
-
+    private String userString, passwordString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,10 +31,21 @@ public class MainActivity extends AppCompatActivity {
 
     }   // Main Method
         public void  clickSingIn(View view){
+            userString        = userEditText.getText().toString().trim();
+            passwordString    = passwordEditText.getText().toString().trim();
+
+            //Check Space
+            if (userString.equals("") || passwordString.equals("")) {
+                //Have Space
+                MyAlert myAlert = new MyAlert();
+                myAlert.myDialog(this,"Have Space","Please Fill All Every Blank");
+
+            }else{
+                //No Space
+
+            }   //if
 
         }//clickSign
-
-
 
 
     public void clikeSignUpMain(View view) {
