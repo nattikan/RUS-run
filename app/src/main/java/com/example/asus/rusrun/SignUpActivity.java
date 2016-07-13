e6a05f6175262841a6d13f900582a0623903cd0a
nattikan <aoaeenattikan@gmail.com>
2016-07-13 11:10:25 +0700

    check chose

--- app/src/main/java/com/example/asus/rusrun/SignUpActivity.java
+++ app/src/main/java/com/example/asus/rusrun/SignUpActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -33,6 +34,33 @@ public class SignUpActivity extends AppCompatActivity {
         avata3RadioButton = (RadioButton) findViewById(R.id.radioButton4);
         avata4RadioButton = (RadioButton) findViewById(R.id.radioButton5);
 
+        //Redio Controller
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                switch (i) {
+                    case R.id.radioButton:
+                        avataString = "0";
+                        break;
+                    case R.id.radioButton2:
+                        avataString = "1";
+                        break;
+                    case R.id.radioButton3:
+                        avataString = "2";
+                        break;
+                    case R.id.radioButton4:
+                        avataString = "3";
+                        break;
+                    case R.id.radioButton5:
+                        avataString = "4";
+                        break;
+
+
+                }  //switch
+            }
+        });
+
 
     } //Main Method
 
@@ -49,8 +77,30 @@ public class SignUpActivity extends AppCompatActivity {
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this,"มีช่องว่าง","กรุณากรอกทุกช่อง");
 
+        } else if (checkChoose()) {
+            //Checken
+        } else {
+            //Un check
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this,"ยังไม่เลือก Avata","กรุณาเลือก Avata");
+
+
         }
+
+
     } // clickSign
+
+    private boolean checkChoose() {
+        boolean status = true;
+
+        status = avata0RadioButton.isChecked() ||
+                 avata1RadioButton.isChecked() ||
+                 avata2RadioButton.isChecked() ||
+                 avata3RadioButton.isChecked() ||
+                 avata4RadioButton.isChecked();
+
+        return status;
+    }
 
 
 } //Main class
