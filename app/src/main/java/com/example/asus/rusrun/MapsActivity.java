cdce1af8760fafa2b299411b9b40d495657bf576
nattikan <aoaeenattikan@gmail.com>
2016-07-15 16:15:30 +0700

    Finish 15 july

--- app/src/main/java/com/example/asus/rusrun/MapsActivity.java
+++ app/src/main/java/com/example/asus/rusrun/MapsActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.okhttp.Call;
@@ -38,6 +39,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LocationManager locationManager;
     private Criteria criteria;
     private double latUserADouble, lngUserADouble;
+    private boolean distanceABoolean = true;
 
 
 
@@ -60,6 +62,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
     }//Main  Method
+
+    //นี่คือ เมทอด ที่หาระยะ ระหว่างจุด
+    private double distance(double lat1, double lon1, double lat2, double lon2) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515 * 1.609344 *1000;//meter Unit
+
+
+        return (dist);
+    }
+
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
+    }
 
     @Override
     protected void onResume() {
@@ -159,6 +181,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         private Context context;
         private GoogleMap googleMap;
         private String urlJSON = "http://swiftcodingthai.com/rus/get_user_nattikan.php";
+        private int[] avataInts = new int[]{R.drawable.bird48, R.drawable.doremon48,
+                R.drawable.kon48, R.drawable.nobita48, R.drawable.rat48};
+
 
         public CreatMarker(Context context, GoogleMap googleMap) {
             this.context = context;
@@ -201,12 +226,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     double douLat = Double.parseDouble(jsonObject.getString("Lat"));
                     double douLng = Double.parseDouble(jsonObject.getString("Lng"));
                     String strName = jsonObject.getString("Name");
+                    int intIndex = Integer.parseInt(jsonObject.getString("Avata"));
+
 
                     LatLng latLng = new LatLng(douLat, douLng);
                     googleMap.addMarker(new MarkerOptions()
                             .position(latLng)
-                            .title(strName));
+                            .title(strName)
 
+                    .icon(BitmapDescriptorFactory.fromResource(avataInts[intIndex])));
 
                 }//for
 
@@ -215,8 +243,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }//onPost
 
-        //CreatMarker Class
 
+
+        //CreatMarker Class
 
 
     }
@@ -233,6 +262,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.clear();
         CreatMarker creatMarker = new CreatMarker(this, mMap);
         creatMarker.execute();
+
+        //CheckDistance
+        double latCheckPoint = 13.859132;
+        double lngCheckPoint = 100.481989;
+        double userDistance = distance(latCheckPoint, lngCheckPoint,
+                latUserADouble, lngUserADouble);
+        Log.d("RusV5", "Distance ==>" + userDistance);
+
+        if (userDistance < 10 ) {
+
+            if (distanceABoolean) {
+
+                MyAlert myAlert = new MyAlert();
+                myAlert.myDialog(this,"ถึงฐานแล้ว","คุณเข้าใกล้ต่ำกว่า 10 เมตรแล้ว");
+                distanceABoolean = false;
+            }
+
+        }// if1
 
 
 
