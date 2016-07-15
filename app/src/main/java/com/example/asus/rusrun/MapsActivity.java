5c4d117c2963a81823ff1256ceb3f8a29a22f9fb
nattikan <aoaeenattikan@gmail.com>
2016-07-15 11:04:17 +0700

    GPS lat log 15 july

--- app/src/main/java/com/example/asus/rusrun/MapsActivity.java
+++ app/src/main/java/com/example/asus/rusrun/MapsActivity.java
@@ -1,7 +1,14 @@
 package com.example.asus.rusrun;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,19 +22,111 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private double latRunADouble = 13.859132;
     private double lngRunADouble = 100.481989;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private double latUserADouble, lngUserADouble;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        //Setup location Service
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+    }//Main  Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        locationManager.removeUpdates(locationListener);
+
+        latUserADouble = latRunADouble;
+        lngUserADouble = lngRunADouble;
+
+        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (networkLocation != null) {
+            latUserADouble = networkLocation.getLatitude();
+            lngUserADouble = networkLocation.getLongitude();
+
+        }
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+
+            latUserADouble = gpsLocation.getLatitude();
+            lngUserADouble = gpsLocation.getLongitude();
+        }
+
+
+
+
+
+    }// onResume
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        locationManager.removeUpdates(locationListener);
+
+
+
     }
 
-    // Main Class
+    public Location myFindLocation (String strProvider) {
 
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+
+            locationManager.requestLocationUpdates(strProvider, 1000,10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+
+
+        } else {
+            Log.d("RusV2", "Cannot Find Location");
+        }
+
+        return location;
+    }
+
+    //Create Class
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            latUserADouble = location.getLatitude();
+            lngRunADouble = location.getLongitude();
+
+
+        } //onLocationChange
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+        }
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -36,5 +135,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng latLng = new LatLng(latRunADouble,lngRunADouble);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
 
+        //Loop
+        myLoop();
+
     }//onMapReady
+
+    private void myLoop() {
+
+        //To Do
+        Log.d("RusV3", "latUser ==>" + latUserADouble);
+        Log.d("RusV3", "lngUser ==>" + lngUserADouble);
+
+
+        //Delay
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               myLoop();
+            }
+        },3000);
+    } //myLoop
+
 }// Main Class
